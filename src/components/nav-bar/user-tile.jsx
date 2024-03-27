228f056c32ebb8ca792c06dbc5ce76595d8f9fb2
Koder2704 <danielkembeu82@gmail.com>
2024-03-27 16:00:44 +0100

    DETAILS PAGE OK

--- src/components/nav-bar/user-tile.jsx
+++ src/components/nav-bar/user-tile.jsx
@@ -2,15 +2,26 @@ import React from 'react';
 import one from '../../assets/1.jpg';
 import { useNavigate } from 'react-router-dom';
 
+
+export function ProfilePictureComponent({ image, width, height, rounded, roundedValue }) {
+  return (
+    <div className={`${rounded ? 'rounded-full' : 'rounded-' + roundedValue} w-${width} h-${height} overflow-hidden`}>
+      <img className='w-full h-full' src={image} alt='User profile' />
+    </div>
+  )
+}
+
 export default function UserTileComponent({ user = { pp: one, name: 'Daniel KEMBEU', role: 'Software Dev - UI/UX Designer' } }) {
 
   const router = useNavigate();
 
   return (
     <div onClick={() => router(`/profile/${user.name.replace(' ', '_').toLowerCase()}`)} className='flex items-center gap-3 m-2'>
-      <div className='rounded-full w-[40px] h-[40px] overflow-hidden'>
-        <img className='w-full h-full' src={user.pp} alt='User profile' />
-      </div>
+      <ProfilePictureComponent
+        rounded
+        width={'[40px]'}
+        height={'[40px]'}
+        image={user.pp} />
 
       <div>
         <h4 className='font-bold text-gray-600 m-0 text-sm'>{user.name}</h4>
--- src/pages/user-profile.jsx
+++ src/pages/user-profile.jsx
@@ -1,7 +1,75 @@
-import React from 'react'
+import React from 'react';
+import img from '../assets/3.jpg';
+import { ProfilePictureComponent } from '../components/nav-bar/user-tile';
+import { useParams } from 'react-router-dom';
 
 export default function UserProfilePage() {
-  return (
-    <div>UserProfilePage</div>
-  )
+
+    const { username } = useParams();
+
+    const [user, setUser] = React.useState();
+
+    React.useEffect(() => {
+        (
+            async () => {
+                try {
+                    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+                    const datas = await response.json();
+
+                    const formattedUsername = username.replace('_', ' ')
+                    setUser(datas.find(user => user.name.toLowerCase() === formattedUsername));
+
+                    console.table(user);
+                } catch (error) {
+                    alert(error.message);
+                }
+            }
+        )()
+    }, [username]);
+
+
+    return (
+        <div className='p-14 w-full h-full grid grid-cols-2'>
+
+            <div className=''>
+                <ProfilePictureComponent
+                    image={img}
+                    width={'[260px]'}
+                    height={'[260px]'}
+                    rounded={false}
+                    roundedValue='xl'
+                />
+                {/* <input ref={imageRef} className='mt-6' type="file" placeholder='Update picture' /> */}
+            </div>
+
+            <div className='flex justify-start w-full'>
+                <ul className='flex flex-col gap-6'>
+                    <li>
+                        <h4 className='text-lg font-bold text-gray-500'>Fullname: </h4>
+                        <p className='text-2xl font-bold text-gray-400'>{user?.name}</p>
+                    </li>
+                    <li>
+                        <h4 className='text-lg font-bold text-gray-500'>Company: </h4>
+                        <p className='text-2xl font-bold text-gray-400'>{user?.company.name}</p>
+                    </li>
+                    <li>
+                        <h4 className='text-lg font-bold text-gray-500'>Phone: </h4>
+                        <p className='text-2xl font-bold text-gray-400'>{user?.phone}</p>
+                    </li>
+                    <li>
+                        <h4 className='text-lg font-bold text-gray-500'>Email: </h4>
+                        <p className='text-2xl font-bold text-gray-400'>{user?.email}</p>
+                    </li>
+                    <li>
+                        <h4 className='text-lg font-bold text-gray-500'>Address: </h4>
+                        <p className='text-2xl font-bold text-gray-400'>{user?.address.street}</p>
+                    </li>
+                    <li>
+                        <h4 className='text-lg font-bold text-gray-500'>Website: </h4>
+                        <p className='text-2xl font-bold text-gray-400'>{user?.website}</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
 }
